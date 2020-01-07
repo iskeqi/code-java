8250c1a8a2be4e901ddf1991c0e91cd2c61c3fe5
iskeqi <iskeqi@126.com>
2020-01-07 15:14:09 +0800

    :sparkles: SpringBoot整合knife4j示例

---
+++ 005-springboot-knife4j/springboot-knife4j/src/test/java/com/keqi/springbootknife4j/SpringbootKnife4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.springbootknife4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKnife4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
