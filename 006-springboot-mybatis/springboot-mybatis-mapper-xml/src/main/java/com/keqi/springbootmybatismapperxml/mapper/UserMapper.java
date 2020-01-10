84f4000e2850bf33725cb2920289faf04b2c2a11
iskeqi <iskeqi@126.com>
2020-01-10 16:39:54 +0800

    :sparkles: SpringBoot整合MyBatis示例

--- 006-springboot-mybatis/springboot-mybatis-mapper-xml/src/main/java/com/keqi/springbootmybatismapperxml/mapper/UserMapper.java
+++ 006-springboot-mybatis/springboot-mybatis-mapper-xml/src/main/java/com/keqi/springbootmybatismapperxml/mapper/UserMapper.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public interface UserMapper {
 
-	//=======================测试增删改方法的使用=========================================//
+	//=======================1、测试增删改方法的使用=========================================//
 
 	/*
 		其中insert/delete/update类型的接口方法可以不设置方法返回值，也可以设置
@@ -22,7 +22,7 @@ public interface UserMapper {
 
 	int updateUserById(User user);
 
-	//=======================测试基本查询方法的使用==================================//
+	//=======================2、测试基本查询方法的使用==================================//
 
 	// 只有select类型的方法返回值，才是真正查询出来的记录封装成的对象
 
@@ -30,21 +30,26 @@ public interface UserMapper {
 
 	User selectUserByUsername(String username);
 
-	//=======================测试方法参数的映射===========================================//
+	//=======================3、测试方法参数的映射===========================================//
 
-	void singleMethodParam(String username);
+	User singleMethodParam(String username);
 
-	void singleEntityParam(User user);
+	User singleEntityParam(User user);
 
-	void multiMethodParam(@Param("id") Long id, @Param("username") String username);
+	// 多个参数必须使用@Param注解指定参数的别名，否则mybatis无法解析
+	User multiMethodParam(@Param("id") Long id, @Param("username") String username);
 
-	void multiEntityParam(@Param("user1") User user1, @Param("user2") User user2);
+	// 多个参数必须使用@Param注解指定参数的别名，否则mybatis无法解析
+	User multiEntityParam(@Param("user1") User user1, @Param("user2") User user2);
 
-	void listParam(List<User> userList);
+	// 批量增加记录(使用List类型的参数)
+	int listParam(List<User> userList);
 
-	void setParam(Set<User> userSet);
+	// 批量增加记录(使用Set类型的参数)
+	int setParam(Set<User> userSet);
 
-	void arrayParam(User[] users);
+	// 批量增加记录(使用Array类型的参数)
+	int arrayParam(User[] users);
 
 	//===========================================================================//
 
