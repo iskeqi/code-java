6c8c10dcd63179900fcd18867eee6e728e09ca0f
iskeqi <iskeqi@126.com>
2020-01-13 23:33:17 +0800

    :sparkles: SpringBoot程序中使用Actuator模块示例

---
+++ 010-springboot-acuator/springboot-actuator-native/src/main/java/com/keqi/springbootactuatornative/SpringbootActuatorNativeApplication.java
@@ -0,0 +1,13 @@
+package com.keqi.springbootactuatornative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuatorNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootActuatorNativeApplication.class, args);
+	}
+
+}
---
+++ 010-springboot-acuator/springboot-actuator-native/src/test/java/com/keqi/springbootactuatornative/SpringbootActuatorNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.springbootactuatornative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActuatorNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
