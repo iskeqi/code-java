c5b5dbb03fe3bf3073e8bb419e188d63a407386a
iskeqi <iskeqi@126.com>
2020-01-13 18:46:55 +0800

    :sparkles: 测试Hutool类库中相关工具类示例

---
+++ 008-hutool/hutool-core/src/main/java/com/keqi/hutool/core/Main.java
@@ -0,0 +1,7 @@
+package com.keqi.hutool.core;
+
+public class Main {
+	public static void main(String[] args) {
+		// 类的具体方法
+	}
+}
---
+++ 008-hutool/hutool-core/src/test/java/com/keqi/hutool/core/DateUtilTest.java
@@ -0,0 +1,36 @@
+package com.keqi.hutool.core;
+
+import cn.hutool.core.date.DateUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 测试
+ */
+public class DateUtilTest {
+
+	@Test
+	public void nowTest() {
+		// 当前时间
+		Date date = DateUtil.date();
+		Assert.assertNotNull(date);
+		// 当前时间
+		Date date2 = DateUtil.date(Calendar.getInstance());
+		Assert.assertNotNull(date2);
+		// 当前时间
+		Date date3 = DateUtil.date(System.currentTimeMillis());
+		Assert.assertNotNull(date3);
+
+		// 当前日期字符串，格式：yyyy-MM-dd HH:mm:ss
+		String now = DateUtil.now();
+		Assert.assertNotNull(now);
+		// 当前日期字符串，格式：yyyy-MM-dd
+		String today = DateUtil.today();
+		Assert.assertNotNull(today);
+	}
+
+
+}
