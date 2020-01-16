4f11d6a0f1c7cc7a30df3c037c332974368434e4
iskeqi <iskeqi@126.com>
2020-01-16 14:55:30 +0800

    :sparkles: fastjson相关API使用示例

---
+++ 013-fastjson/src/test/java/com/keqi/fastjson/FastJsonTest.java
@@ -0,0 +1,13 @@
+package com.keqi.fastjson;
+
+public class FastJsonTest {
+
+
+	public void test() {
+
+
+		// String s = JSON.toJSONString(response2, SerializerFeature.WriteMapNullValue);
+		// System.out.println(s); // 可见设置了这个属性后，原来默认为null的字段，现在已经可以输出了
+	}
+
+}
