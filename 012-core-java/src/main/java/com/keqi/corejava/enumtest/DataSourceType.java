d1b71b2e0921d166b01d17c36051baec888e69af
iskeqi <iskeqi@126.com>
2020-01-16 14:51:51 +0800

    :sparkles: java-core相关API使用示例

---
+++ 012-core-java/src/main/java/com/keqi/corejava/enumtest/DataSourceType.java
@@ -0,0 +1,23 @@
+package com.keqi.corejava.enumtest;
+
+/**
+ * 数据源(数据源枚举类，枚举类即使不指定属性，也是可以直接用的，
+ * 就相当于是一个字符串属性，只是不用显示的写出来了
+ * )
+ *
+ * @author ruoyi
+ */
+public enum DataSourceType {
+
+	/**
+	 * 主库
+	 */
+	MASTER,
+
+	/**
+	 * 从库
+	 */
+	SLAVE
+
+}
+
---
+++ 012-core-java/src/test/java/com/keqi/corejava/enumtest/EnumTest.java
@@ -0,0 +1,12 @@
+package com.keqi.corejava.enumtest;
+
+import org.junit.Test;
+
+public class EnumTest {
+
+	@Test
+	public void test() {
+		System.out.println(DataSourceType.MASTER);
+	}
+
+}
