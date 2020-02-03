2144f978cb3eb7ee06f052edc052d2bc9f824a3b
iskeqi <iskeqi@126.com>
2020-02-03 12:31:23 +0800

    :sparkles: SpringBoot构建的后台种子项目示例

---
+++ 009-springboot-project-seed/idomp-bjzjc/idomp-web/src/main/java/com/qjzh/idomp/zjc/common/AjaxEntity.java
@@ -0,0 +1,24 @@
+package com.qjzh.idomp.zjc.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 响应实体类
+ *
+ * @author keqi
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AjaxEntity<T> {
+
+	private Integer status;
+
+	private String msg;
+
+	private T body;
+}
---
+++ 009-springboot-project-seed/idomp-bjzjc/idomp-web/src/main/java/com/qjzh/idomp/zjc/common/AjaxPageEntity.java
@@ -0,0 +1,34 @@
+package com.qjzh.idomp.zjc.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * 分页响应实体类
+ *
+ * @author keqi
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AjaxPageEntity<T> {
+
+	private Integer status;
+
+	private String msg;
+
+	private PageEntity<T> body;
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public class PageEntity<T> {
+
+		private Long total;
+
+		private List<T> records;
+	}
+}
