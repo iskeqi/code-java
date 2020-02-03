62ddb62fd7eb2c3a27cee532ac68d2174229104d
iskeqi <iskeqi@126.com>
2020-02-03 12:04:21 +0800

    :sparkles: SpringBoot构建的后台种子项目示例

---
+++ 009-springboot-project-seed/idomp-bjzjc/idomp-web/src/main/java/com/qjzh/idomp/zjc/IdompWebApplication.java
@@ -0,0 +1,13 @@
+package com.qjzh.idomp.zjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdompWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdompWebApplication.class, args);
+	}
+
+}
---
+++ 009-springboot-project-seed/idomp-bjzjc/idomp-web/src/test/java/com/qjzh/idomp/zjc/IdompWebApplicationTests.java
@@ -0,0 +1,8 @@
+package com.qjzh.idomp.zjc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdompWebApplicationTests {
+
+}
