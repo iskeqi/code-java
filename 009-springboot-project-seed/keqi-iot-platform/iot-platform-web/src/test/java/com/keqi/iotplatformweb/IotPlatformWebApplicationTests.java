ea0e3c577e8016330e2a1fee6db6a1fafb6495ab
iskeqi <iskeqi@126.com>
2020-02-23 00:34:37 +0800

    :sparkles: SpringBoot构建的后台种子项目示例

---
+++ 009-springboot-project-seed/keqi-iot-platform/iot-platform-web/src/test/java/com/keqi/iotplatformweb/IotPlatformWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.iotplatformweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotPlatformWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
