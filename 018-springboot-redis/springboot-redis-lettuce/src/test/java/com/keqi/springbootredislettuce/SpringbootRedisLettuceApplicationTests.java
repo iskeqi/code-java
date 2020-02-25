8abdc6856658d5e924257db7dffb5a6ef587d829
iskeqi <iskeqi@126.com>
2020-02-25 18:46:35 +0800

    :sparkles: SpringBoot程序中使用Redis的各种示例

---
+++ 018-springboot-redis/springboot-redis-lettuce/src/test/java/com/keqi/springbootredislettuce/SpringbootRedisLettuceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.springbootredislettuce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisLettuceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
