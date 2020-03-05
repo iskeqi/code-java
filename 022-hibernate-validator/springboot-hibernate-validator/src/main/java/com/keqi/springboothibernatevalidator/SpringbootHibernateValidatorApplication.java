f715fd72976122d51ef25ad44ac9857706915cbb
iskeqi <iskeqi@126.com>
2020-03-05 22:47:31 +0800

    :sparkles: hibernate-validator 使用示例

---
+++ 022-hibernate-validator/springboot-hibernate-validator/src/main/java/com/keqi/springboothibernatevalidator/SpringbootHibernateValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.keqi.springboothibernatevalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateValidatorApplication.class, args);
+	}
+
+}
---
+++ 022-hibernate-validator/springboot-hibernate-validator/src/test/java/com/keqi/springboothibernatevalidator/SpringbootHibernateValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.springboothibernatevalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHibernateValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
