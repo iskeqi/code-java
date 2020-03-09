52d066304aea5de695a305196ce2a1e1aca2c35a
iskeqi <iskeqi@126.com>
2020-03-09 23:00:34 +0800

    :sparkles: SpringBoot构建的后台种子项目示例

---
+++ 009-springboot-project-seed/dict-sample/spirngboot-dict-db/src/main/java/com/keqi/spirngbootdictdb/SpirngbootDictDbApplication.java
@@ -0,0 +1,13 @@
+package com.keqi.spirngbootdictdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngbootDictDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngbootDictDbApplication.class, args);
+	}
+
+}
---
+++ 009-springboot-project-seed/dict-sample/spirngboot-dict-db/src/test/java/com/keqi/spirngbootdictdb/SpirngbootDictDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.spirngbootdictdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngbootDictDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
