6594643963e5a53b7643b571d392046b039a7c76
iskeqi <iskeqi@126.com>
2020-03-10 18:54:58 +0800

    :sparkles: java-core相关API使用示例

--- 012-core-java/src/test/java/com/keqi/corejava/enumtest/EnumTest.java
+++ 012-core-java/src/test/java/com/keqi/corejava/enumtest/EnumTest.java
@@ -7,7 +7,6 @@ public class EnumTest {
 	@Test
 	public void test() {
 		System.out.println(DataSourceTypeEnum.MASTER);
-
 	}
 
 	@Test
@@ -17,6 +16,20 @@ public class EnumTest {
 
 		// 如果枚举是没有值的呢？
 		System.out.println(DataSourceTypeEnum.MASTER.equals("MASTER")); // 输出仍然为false
+	}
+
+	@Test
+	public void testEnumName() {
+		String ALL = "ALL";
+
+		// 枚举类是重写了toString() 方法的
+		System.out.println(SampleTypeEnum.ALL.toString());
+		// 通过枚举类的name()方法，也是可以直接获取到对象的名字的那个字符串的哦
+		System.out.println(SampleTypeEnum.ALL.name());
+
+		System.out.println(SampleTypeEnum.ALL.name().equals(ALL)); // 输出为true
+
+		System.out.println(SampleTypeEnum.ALL.toString().equals(ALL)); // 输出为true
 	}
 
 }
