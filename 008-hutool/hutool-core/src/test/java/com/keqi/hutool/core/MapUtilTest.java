472675b9658e63db8ccae553b9e2772aeacc73b8
iskeqi <iskeqi@126.com>
2020-03-12 15:11:48 +0800

    :sparkles: 测试Hutool类库中相关工具类示例

--- 008-hutool/hutool-core/src/test/java/com/keqi/hutool/core/MapUtilTest.java
+++ 008-hutool/hutool-core/src/test/java/com/keqi/hutool/core/MapUtilTest.java
@@ -18,6 +18,7 @@ public class MapUtilTest {
 		objMap.put("username","keqi");
 		objMap.put("password","123456");
 		objMap.put("password2","12G3456");
+		objMap.put("password4","");
 	}
 
 
@@ -36,6 +37,13 @@ public class MapUtilTest {
 		String password3 = MapUtil.getStr(objMap, "password3");
 		// 一个null值，去转换，返回也会是一个null
 		Long aLong3 = Convert.toLong(password3);
+
+		// 如果从Map中获取的key对应的值是一个""
+		String password4 = MapUtil.getStr(objMap, "password4");
+		// 在做类型转换时，把它转成Long类型，那么返回值肯定是null
+		Long aLong4 = Convert.toLong(password4);
+
+		// 总结：cn.hutool.core.convert.Convert 类在无法转换时，并不会抛出异常，而是返回一个null
 
 		System.out.println("-");
 	}
