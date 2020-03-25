258e4f5777842c8bc4dc3a7749870e21a8bed4ea
iskeqi <iskeqi@126.com>
2020-03-26 00:11:43 +0800

    iot-platform 项目框架搭建示例

---
+++ 026-iot-platform/iot-platform-web/src/main/java/com/keqi/iotplatform/IotPlatformWebApplication.java
@@ -0,0 +1,13 @@
+package com.keqi.iotplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotPlatformWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotPlatformWebApplication.class, args);
+	}
+
+}
---
+++ 026-iot-platform/iot-platform-web/src/test/java/com/keqi/iotplatform/IotPlatformWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.iotplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotPlatformWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
