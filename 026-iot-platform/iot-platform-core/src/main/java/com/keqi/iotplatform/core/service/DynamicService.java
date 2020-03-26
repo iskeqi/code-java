b86006b2341df9a8e59d928ecd7385b462fc876d
iskeqi <iskeqi@126.com>
2020-03-26 14:43:53 +0800

    iot-platform 项目框架搭建示例

---
+++ 026-iot-platform/iot-platform-core/src/main/java/com/keqi/iotplatform/core/service/DynamicService.java
@@ -0,0 +1,19 @@
+package com.keqi.iotplatform.core.service;
+
+
+import com.keqi.iotplatform.core.domain.DataSourceBO;
+
+public interface DynamicService {
+
+	/**
+	 * 增加HikariCP连接池数据源
+	 * @param dataSourceBO dataSourceBO
+	 */
+	void addHikariCP(DataSourceBO dataSourceBO);
+
+	/**
+	 * 移除HikariCP连接池数据源
+	 * @param pollName pollName
+	 */
+	void removeHikariCP(String pollName);
+}
