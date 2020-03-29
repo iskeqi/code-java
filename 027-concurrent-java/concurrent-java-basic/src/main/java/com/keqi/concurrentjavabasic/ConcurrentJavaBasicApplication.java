260322228898d861d5c13b64b3f3a6764f817a09
iskeqi <iskeqi@126.com>
2020-03-29 20:39:05 +0800

    :sparkles: java 多线程编写示例

---
+++ 027-concurrent-java/concurrent-java-basic/src/main/java/com/keqi/concurrentjavabasic/ConcurrentJavaBasicApplication.java
@@ -0,0 +1,13 @@
+package com.keqi.concurrentjavabasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentJavaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentJavaBasicApplication.class, args);
+	}
+
+}
---
+++ 027-concurrent-java/concurrent-java-basic/src/test/java/com/keqi/concurrentjavabasic/ConcurrentJavaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.concurrentjavabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentJavaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
