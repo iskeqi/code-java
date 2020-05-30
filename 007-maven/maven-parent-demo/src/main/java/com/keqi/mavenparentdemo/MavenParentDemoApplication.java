22f32f55eb736703a5bb54b95836e79c9215ffbb
a1209023760 <1209023760@qq.com>
2020-05-31 04:05:05 +0800

    :sparkles: Apache Maven使用示例

---
+++ 007-maven/maven-parent-demo/src/main/java/com/keqi/mavenparentdemo/MavenParentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.keqi.mavenparentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenParentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenParentDemoApplication.class, args);
+	}
+
+}
---
+++ 007-maven/maven-parent-demo/src/test/java/com/keqi/mavenparentdemo/MavenParentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.mavenparentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenParentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
