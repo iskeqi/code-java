ab0516562305ff9d86c660a38405cc65a2d69190
keqi <iskeqi@126.com>
2020-12-08 18:00:15 +0800

    :memo: knife4j 使用示例

---
+++ 034-knife4j/src/main/java/com/keqi/knife4j/core/typeHandler/JsonMapHandler.java
@@ -0,0 +1,39 @@
+package com.keqi.knife4j.core.typeHandler;
+
+import com.keqi.knife4j.core.util.JsonUtil;
+import org.apache.ibatis.type.Alias;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+@Alias("JsonMapHandler")
+public class JsonMapHandler extends BaseTypeHandler<Map<String, Object>> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Map<String, Object> parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, JsonUtil.writeValueAsString(parameter));
+    }
+
+    @Override
+    public Map<String, Object> getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        String jsonStr = rs.getString(columnName);
+        return jsonStr == null ? null : JsonUtil.readValue(jsonStr, Map.class);
+    }
+
+    @Override
+    public Map<String, Object> getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        String jsonStr = rs.getString(columnIndex);
+        return jsonStr == null ? null : JsonUtil.readValue(jsonStr, Map.class);
+    }
+
+    @Override
+    public Map<String, Object> getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        String jsonStr = cs.getString(columnIndex);
+        return jsonStr == null ? null : JsonUtil.readValue(jsonStr, Map.class);
+    }
+}
