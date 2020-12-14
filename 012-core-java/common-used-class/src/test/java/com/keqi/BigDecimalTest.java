45645c9b36e3b49e7e0c3b129cbc81eca8c4f485
keqi <iskeqi@126.com>
2020-12-14 14:38:11 +0800

    :sparkles: java-core相关API使用示例

--- 012-core-java/common-used-class/src/test/java/com/keqi/BigDecimalTest.java
+++ 012-core-java/common-used-class/src/test/java/com/keqi/BigDecimalTest.java
@@ -22,5 +22,19 @@ public class BigDecimalTest {
 
         // 除法(UP，保留两位小数)
         System.out.println(a.divide(b, 2, RoundingMode.UP));
+
+        // 比较两个 BigDecimal 数的大小（使用 equals 方法会比较每一个字符串，使用 compareTo 方法比较的才是数字对应的大小）
+        BigDecimal a1 = new BigDecimal("100.000");
+        BigDecimal b1 = new BigDecimal("100");
+        System.out.println(a1.equals(b1)); // 输出为 false
+        System.out.println(a1.compareTo(b1)); // 输出为 0
+        System.out.println(a1.compareTo(b1) == 0); // 输出为 true
+
+        // 只要存在浮点数，就使用 BigDecimal 来处理就行，禁止在程序中使用 Float 和 Double
+        // MySQL 中浮点数直接使用 decimal 类型，在序列化VO到前端时，可以通过@JsonSerialize注解的using属性指定序列化方式，然后自定义一个序列化类即可，
+        // 参考链接如下：https://segmentfault.com/a/1190000022755526
+        //            https://blog.csdn.net/weter_drop/article/details/103374041
+        BigDecimal a2 = new BigDecimal("100.0320040000");
+        System.out.println(a2.stripTrailingZeros()); // 去掉后缀中的0
     }
 }
