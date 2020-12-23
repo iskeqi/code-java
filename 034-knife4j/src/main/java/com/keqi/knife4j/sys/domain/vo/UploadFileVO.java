796d431037fd005c28bad3259f51e8933304b5c0
keqi <iskeqi@126.com>
2020-12-23 15:51:13 +0800

    :memo: knife4j 使用示例

--- 034-knife4j/src/main/java/com/keqi/knife4j/sys/domain/vo/UploadFileVO.java
+++ 034-knife4j/src/main/java/com/keqi/knife4j/sys/domain/vo/UploadFileVO.java
@@ -12,10 +12,10 @@ public class UploadFileVO {
     @ApiModelProperty(value = "文件ID", example = "1")
     private Long id;
 
-    @ApiModelProperty(value = "文件名称", example = "字典数据表")
+    @ApiModelProperty(value = "文件名称", example = "85b4f070-d238-4e34-bd40-d72885a42f6fSnipaste_2020-12-23_11-23-11.png")
     private String name;
 
-    @ApiModelProperty(value = "文件存储路径（相对路径）", example = "/sl")
+    @ApiModelProperty(value = "文件存储路径（相对路径）", example = "/2020-12-23/image/png/")
     private String path;
 
     public UploadFileVO() {
