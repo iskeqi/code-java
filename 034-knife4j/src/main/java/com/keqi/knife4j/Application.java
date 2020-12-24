47631a9086914f78c5767f898dcc7e3a3dea278f
keqi <iskeqi@126.com>
2020-12-24 17:44:11 +0800

    :memo: knife4j 使用示例

--- 034-knife4j/src/main/java/com/keqi/knife4j/Application.java
+++ 034-knife4j/src/main/java/com/keqi/knife4j/Application.java
@@ -4,13 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Slf4j
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 		SpringApplication.run(Application.class, args);
-		log.debug("API 文档地址：http://localhost:9090/knife4j/doc.html");
+		log.debug("API 文档地址：http://{}:9090/knife4j/doc.html", InetAddress.getLocalHost().getHostAddress());
 	}
 
 }
