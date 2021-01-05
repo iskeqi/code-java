0a86eee708467e571d2e81f0be64f0c4cede5d08
keqi <iskeqi@126.com>
2021-01-05 09:26:57 +0800

    :sparkles: 测试Hutool类库中相关工具类示例

--- 008-hutool/hutool-http/src/main/java/com/keqi/http/Main.java
+++ 008-hutool/hutool-http/src/main/java/com/keqi/http/Main.java
@@ -2,8 +2,8 @@ package com.keqi.http;
 
 public class Main {
 	public static void main(String[] args) {
-		// 简单的直接使用 HttpUtil 工具类
+		// 简单的直接使用 HttpUtil 工具类的 get/post 方法即可
 
-		// 需要获取 HttpResponse 的，就使用 HttpRequest 来执行 HTTP 请求
+		// 如果需要获取 HttpResponse 对象的话，就使用 HttpRequest 的 execute() 方法来执行 HTTP 请求
 	}
 }
