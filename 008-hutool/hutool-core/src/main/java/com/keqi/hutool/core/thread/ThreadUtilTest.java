ad0c44e8970e3b83ef7783bfcf03b3f9df4f32ac
keqi <iskeqi@126.com>
2021-01-14 14:28:13 +0800

    :sparkles: 测试Hutool类库中相关工具类示例

--- 008-hutool/hutool-core/src/main/java/com/keqi/hutool/core/thread/ThreadUtilTest.java
+++ 008-hutool/hutool-core/src/main/java/com/keqi/hutool/core/thread/ThreadUtilTest.java
@@ -1,12 +1,26 @@
 package com.keqi.hutool.core.thread;
 
+import cn.hutool.core.util.NumberUtil;
+
+import java.math.BigDecimal;
+
 /**
  * 测试 cn.hutool.core.thread.ThreadUtil 相关 API 使用
  */
 public class ThreadUtilTest {
 
 	public static void main(String[] args) {
-
+		System.out.println(generateProcessingTimeLimit(0, 0));
 	}
 
+	private static String generateProcessingTimeLimit(Integer hour, Integer minute) {
+		if (hour == null || minute == null) {
+			return "";
+		}
+
+
+		BigDecimal div = NumberUtil.div(String.valueOf(minute), String.valueOf(60), 2);
+		BigDecimal sum = NumberUtil.add(div, new BigDecimal(hour));
+		return sum.toString() + "h";
+	}
 }
