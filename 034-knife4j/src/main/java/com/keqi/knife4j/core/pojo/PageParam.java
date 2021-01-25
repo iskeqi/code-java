a03a4381f36049341df8e83febef44270fe44b19
keqi <iskeqi@126.com>
2021-01-25 18:12:04 +0800

    :memo: knife4j 使用示例

--- 034-knife4j/src/main/java/com/keqi/knife4j/core/pojo/PageParam.java
+++ 034-knife4j/src/main/java/com/keqi/knife4j/core/pojo/PageParam.java
@@ -2,30 +2,33 @@ package com.keqi.knife4j.core.pojo;
 
 import io.swagger.annotations.ApiModelProperty;
 
+/**
+ * 命名和 PageHelper 保持一致
+ */
 public class PageParam {
 
-	@ApiModelProperty(value = "前页数", example = "1", required = true)
-	protected int pageNum = 1;
+    @ApiModelProperty(value = "前页数", example = "1", required = true)
+    protected int pageNum = 1;
 
-	@ApiModelProperty(value = "每页大小", example = "10", required = true)
-	protected int pageSize = 10;
+    @ApiModelProperty(value = "每页大小", example = "10", required = true)
+    protected int pageSize = 10;
 
-	public PageParam() {
-	}
+    public PageParam() {
+    }
 
-	public int getPageNum() {
-		return pageNum;
-	}
+    public int getPageNum() {
+        return pageNum;
+    }
 
-	public void setPageNum(int pageNum) {
-		this.pageNum = pageNum;
-	}
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
 
-	public int getPageSize() {
-		return pageSize;
-	}
+    public int getPageSize() {
+        return pageSize;
+    }
 
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
 }
--- 034-knife4j/src/main/java/com/keqi/knife4j/core/pojo/PageVO.java
+++ 034-knife4j/src/main/java/com/keqi/knife4j/core/pojo/PageVO.java
@@ -5,7 +5,7 @@ import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 
 /**
- * 分页响应VO（命名和 MyBatisPlus 保持一致）
+ * 分页响应VO（命名和 PageHelper 保持一致）
  *
  * @author keqi
  */
@@ -15,14 +15,14 @@ public class PageVO<T> {
 	private long total;
 
 	@ApiModelProperty(value = "记录列表")
-	private List<T> records;
+	private List<T> list;
 
 	public PageVO() {
 	}
 
-	public PageVO(long total, List<T> records) {
+	public PageVO(long total, List<T> list) {
 		this.total = total;
-		this.records = records;
+		this.list = list;
 	}
 
 	public long getTotal() {
@@ -33,11 +33,11 @@ public class PageVO<T> {
 		this.total = total;
 	}
 
-	public List<T> getRecords() {
-		return records;
+	public List<T> getList() {
+		return list;
 	}
 
-	public void setRecords(List<T> records) {
-		this.records = records;
+	public void setList(List<T> list) {
+		this.list = list;
 	}
 }
