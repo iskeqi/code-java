41748d1ee6bc677465933f4b0ac6a00ac87e5f9a
keqi <iskeqi@126.com>
2021-02-01 22:55:24 +0800

    :memo: SpringBoot MVC 模块使用示例

---
+++ 023-springboot-mvc/springboot-mvc-static-resource/src/main/java/com/keqi/springbootmvcstaticresource/SpringbootMvcStaticResourceApplication.java
@@ -0,0 +1,21 @@
+package com.keqi.springbootmvcstaticresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootMvcStaticResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMvcStaticResourceApplication.class, args);
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "bbb";
+    }
+
+}
---
+++ 023-springboot-mvc/springboot-mvc-static-resource/src/test/java/com/keqi/springbootmvcstaticresource/SpringbootMvcStaticResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.springbootmvcstaticresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcStaticResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
