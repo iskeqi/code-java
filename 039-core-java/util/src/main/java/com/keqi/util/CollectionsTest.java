9e64657f256269b8a6a847946440336d3bde5044
keqi <iskeqi@126.com>
2021-02-01 19:43:35 +0800

    :sparkles: java-core相关API使用示例

---
+++ 039-core-java/util/src/main/java/com/keqi/util/CollectionsTest.java
@@ -0,0 +1,14 @@
+package com.keqi.util;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsTest {
+
+    public static void main(String[] args) {
+        // 这个方法非常适合用来返回一个空的集合，避免NPE问题
+        List emptyList = Collections.emptyList();
+
+        System.out.println(emptyList.size());
+    }
+}
