fd3f0b422703ac2a26f718e4a58262528f569ec2
keqi <iskeqi@126.com>
2021-02-22 17:26:22 +0800

    :memo: oss-storage 使用示例

---
+++ 044-oss-storage/oss-storage-aliyun/src/main/java/com/keqi/ossstoragealiyun/OssStorageAliyunApplication.java
@@ -0,0 +1,13 @@
+package com.keqi.ossstoragealiyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssStorageAliyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssStorageAliyunApplication.class, args);
+    }
+
+}
---
+++ 044-oss-storage/oss-storage-aliyun/src/test/java/com/keqi/ossstoragealiyun/OssStorageAliyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.ossstoragealiyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssStorageAliyunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
