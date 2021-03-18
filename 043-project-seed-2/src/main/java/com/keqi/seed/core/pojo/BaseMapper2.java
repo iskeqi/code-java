fe915d26ec8c5570a88da2422062152df52ab5b7
keqi <iskeqi@126.com>
2021-03-18 12:30:24 +0800

    :memo: project-seed-2 使用示例

--- 043-project-seed-2/src/main/java/com/keqi/seed/core/pojo/BaseMapper2.java
+++ 043-project-seed-2/src/main/java/com/keqi/seed/core/pojo/BaseMapper2.java
@@ -9,12 +9,6 @@ public interface BaseMapper2<DO extends BaseDO> {
 
     int insert(DO t);
 
-    int delete(DO condition);
-
-    int update(@Param("t") DO t, @Param("con") DO condition);
-
-    List<DO> findList(DO condition);
-
     default int insertList(@Param("list") List<DO> list) {
         int count = 0;
         if (!CollectionUtils.isEmpty(list)) {
@@ -25,38 +19,11 @@ public interface BaseMapper2<DO extends BaseDO> {
         return count;
     }
 
-    default int deleteById(long id) {
-        // 这种方式运行时会报错
-        return this.delete((DO) new BaseDO(id));
-    }
+    int delete(DO condition);
 
-    default int deleteByIds(@Param("ids") List<Long> ids) {
-        int count = 0;
-        if (!CollectionUtils.isEmpty(ids)) {
-            for (Long id : ids) {
-                count += this.deleteById(id);
-            }
-        }
-        return count;
-    }
+    int update(@Param("t") DO t, @Param("con") DO condition);
 
-    default int updateById(DO t) {
-        BaseDO id;
-        try {
-            id = t.getClass().newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
-            throw new RuntimeException("Cannot instantiate object with no-argument constructor");
-        }
-        id.setId(t.getId());
-
-        t.setId(null);
-        return this.update(t, (DO) id);
-    }
-
-    default DO findById(long id) {
-        // 这种方式运行时会报错
-        return this.find((DO) new BaseDO(id));
-    }
+    List<DO> findList(DO condition);
 
     default DO find(DO condition) {
         List<DO> list = this.findList(condition);
