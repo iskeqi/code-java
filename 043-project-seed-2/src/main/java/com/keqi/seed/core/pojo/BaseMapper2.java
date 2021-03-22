6b2698ea4a04e50024dfa34ba7717a8d174ed590
keqi <iskeqi@126.com>
2021-03-22 12:46:17 +0800

    :memo: project-seed-2 使用示例

--- 043-project-seed-2/src/main/java/com/keqi/seed/core/pojo/BaseMapper2.java
+++ 043-project-seed-2/src/main/java/com/keqi/seed/core/pojo/BaseMapper2.java
@@ -5,18 +5,16 @@ import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
-public interface BaseMapper2<DO extends BaseDO> {
+public interface BaseMapper2<DO> {
 
-    int insert(DO t);
+    void insert(DO t);
 
-    default int insertList(@Param("list") List<DO> list) {
-        int count = 0;
+    default void insertList(List<DO> list) {
         if (!CollectionUtils.isEmpty(list)) {
             for (DO t : list) {
-                count += this.insert(t);
+                this.insert(t);
             }
         }
-        return count;
     }
 
     int delete(DO condition);
@@ -31,11 +29,7 @@ public interface BaseMapper2<DO extends BaseDO> {
             throw new RuntimeException("Expect one, actually return multiple results");
         }
         if (list.size() == 0) {
-            try {
-                BaseDO t = condition.getClass().newInstance();
-            } catch (InstantiationException | IllegalAccessException e) {
-                throw new RuntimeException("Cannot instantiate object with no-argument constructor");
-            }
+            return null;
         }
         return list.get(0);
     }
