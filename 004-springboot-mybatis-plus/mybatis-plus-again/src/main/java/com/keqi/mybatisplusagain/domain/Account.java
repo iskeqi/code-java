f2ed18953fee309f78dd6de0fc5857357fdca58b
keqi <iskeqi@126.com>
2021-04-02 11:03:24 +0800

    :sparkles: SpringBoot整合MyBatisPlus示例

--- 004-springboot-mybatis-plus/mybatis-plus-again/src/main/java/com/keqi/mybatisplusagain/domain/Account.java
+++ 004-springboot-mybatis-plus/mybatis-plus-again/src/main/java/com/keqi/mybatisplusagain/domain/Account.java
@@ -19,55 +19,62 @@ public class Account {
 	/**
 	 * 用户id
 	 */
-	@TableId(value = "id", type = IdType.AUTO)
+	@TableId(value = "id",type = IdType.ASSIGN_ID)
+	/*
+		MP 的主键生成策略一共有4种
+			1、NONE/INPUT：默认的机制，需要自己手动（自定义主键生成策略）的去填充主键，或者利用自动填充机制也行（你有比它更好算法的时候，推荐）
+			2、AUTO：依赖于数据库的主键自增策略
+			3、ASSIGN_ID 使用默认的雪花算法生成ID，支持 number 和 string 类型（推荐！！！）
+			4、ASSIGN_UUID 使用UUID生成主键，只支持主键类型为 String 的
+	 */
 	private Long id;
 
 	/**
 	 * 用户名
 	 */
-	//@TableField(value = "account")
+	@TableField(value = "account")
 	private String account;
 
 	/**
 	 * 姓名
 	 */
-	//@TableField(value = "nick_name")
+	@TableField(value = "nick_name")
 	// 默认情况下 MyBatisPlus 会自动的帮你把下划线转成驼峰，只要你代码中的字段和数据库中的字段都严格遵守这种规范
 	private String nickName;
 
 	/**
 	 * 岗位
 	 */
-	//@TableField(value = "post")
+	@TableField(value = "post")
 	private String post;
 
 	/**
 	 * 密码
 	 */
-	//@TableField(value = "`password`")
+	@TableField(value = "`password`")
 	private String password;
 
 	/**
 	 * 盐
 	 */
-	//@TableField(value = "`salt`")
+	@TableField(exist = false)
 	private String salt;
 
 	/**
 	 * 创建时间
 	 */
-	@TableField(/*value = "create_time", */fill = FieldFill.INSERT)
+	@TableField(value = "create_time", fill = FieldFill.INSERT)
 	private LocalDateTime createTime;
 
 	/**
 	 * 修改时间
 	 */
-	@TableField(/*value = "update_time", */fill = FieldFill.INSERT_UPDATE)
+	@TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
 	private LocalDateTime updateTime;
 
 	/**
 	 * 是否删除[0 未删除，1 已删除]
 	 */
-	//@TableField(value = "deleted")
+	@TableField(value = "deleted")
 	private Integer deleted;
 }
--- 004-springboot-mybatis-plus/mybatis-plus-again/src/test/java/com/keqi/mybatisplusagain/MybatisPlusAgainApplicationTests.java
+++ 004-springboot-mybatis-plus/mybatis-plus-again/src/test/java/com/keqi/mybatisplusagain/MybatisPlusAgainApplicationTests.java
@@ -19,7 +19,10 @@ class MybatisPlusAgainApplicationTests {
 		Account account = new Account();
 		account.setAccount("keqi");
 		account.setPassword("123456");
-		account.setNickName("柯琦");
+		account.setNickName("测试 IdType.ASSIGN_ID 类型的主键");
+
+		System.out.println("测试一下这个主键是在创建对象的时候生成的还是执行了 insert 后才生成的：" + account.getId());
+		// 果然是执行了 SQL 之后才生成的，其实这才是合理的，创建对象的时候生成反而是错误的，毕竟创建对象又不是一定要插入到数据库中
 		accountMapper.insert(account);
 	}
 
@@ -38,4 +41,44 @@ class MybatisPlusAgainApplicationTests {
 		System.out.println(page.getRecords().size());
 	}
 
+	@Test
+	void contextLoads3() {
+		// 使用mp的分页插件需要满足两个条件：
+		// 第一个参数必须是 com.baomidou.mybatisplus.extension.plugins.pagination.Page 对象或者它的子类
+		// 第二个是，返回值必须是 IPage<T> 类型
+		PageParam pageParam = new PageParam();
+		pageParam.setCurrent(1);
+		pageParam.setSize(1);
+		pageParam.setNickName("柯琦");
+		IPage<Account> page = accountMapper.page(pageParam);
+
+		System.out.println(page.getTotal());
+		System.out.println(page.getRecords().size());
+	}
+
+	@Test
+	void contextLoads4() {
+		// 测试逻辑删除
+		Long id = 1377814206023647234L;
+		Account account = accountMapper.selectById(id);
+		System.out.println(account.getId());
+	}
+
+	/*
+		MyBatisPlus 提供了很多功能，但是不是每一个都有必要去使用，你需要关注的有以下几个
+
+			1、自动填充（必做）
+			2、逻辑删除（推荐）
+			3、SQL 打印分析（推荐）
+			4、分页插件（必做）
+			5、主键生成策略，最好使用雪花算法{IdType.ASSIGN_ID}，而不是数据库自增id
+
+			6、通用枚举、字段类型处理器（不推荐，并没有必要去使用）
+			7、数据安全保护（不推荐，没必要这么干）
+
+
+
+
+
+	 */
 }
