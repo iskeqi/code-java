aa505ffeafb3925a1e747d2f2dacdb8ff82ad832
keqi <iskeqi@126.com>
2021-04-19 10:37:13 +0800

    :sparkles: java-core相关API使用示例

--- 039-core-java/util/src/main/java/com/keqi/util/CollectionsTest.java
+++ 039-core-java/util/src/main/java/com/keqi/util/CollectionsTest.java
@@ -2,18 +2,29 @@ package com.keqi.util;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class CollectionsTest {
 
-    public static void main(String[] args) {
-        // 这个方法非常适合用来返回一个空的集合，避免NPE问题
-        List emptyList = Collections.emptyList();
+	public static void main(String[] args) {
+		// 这个方法非常适合用来返回一个空的集合，避免NPE问题
+		List emptyList = Collections.emptyList();
 
-        System.out.println(emptyList.size());
+		System.out.println(emptyList.size());
 
-        List emptyList1 = Collections.EMPTY_LIST;
-        List<Object> objects = Collections.emptyList();
+		List emptyList1 = Collections.EMPTY_LIST;
+		List<Object> objects = Collections.emptyList();
 
 
-    }
+		// 构造一个只有一个 value 的集合
+		Set<String> set = Collections.singleton("set");
+		List<String> list = Collections.singletonList("list");
+		Map<String, String> map = Collections.singletonMap("key", "value");
+
+		// 返回一个空的集合
+		List emptyList2 = Collections.EMPTY_LIST;
+		Set emptySet = Collections.EMPTY_SET;
+		Map emptyMap = Collections.EMPTY_MAP;
+	}
 }
