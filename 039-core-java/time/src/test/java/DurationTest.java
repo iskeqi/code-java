07031f02e1d5ebb70da4a657b967c6fb14b26265
keqi <iskeqi@126.com>
2021-04-22 22:44:35 +0800

    :sparkles: java-core相关API使用示例

--- 039-core-java/time/src/test/java/DurationTest.java
+++ 039-core-java/time/src/test/java/DurationTest.java
@@ -1,7 +1,11 @@
 import org.junit.Test;
 
-import java.time.*;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 
 public class DurationTest {
 
@@ -43,6 +47,64 @@ public class DurationTest {
 	}
 
 
+	@Test
+	public void cal() {
+		// 计算 xx 年 xx 月 xx 日 xx 时 xx 分 xx 秒
+		LocalDateTime time1 = LocalDateTime.of(2021, 4, 21, 22, 20, 21);
+		LocalDateTime time2 = LocalDateTime.of(2020, 10, 22, 22, 22, 22);
 
+
+		int year = time1.getYear();
+		int month = time1.getMonthValue();
+		int day = time1.getDayOfMonth();
+		int hours = time1.getHour();
+		int minute = time1.getMinute();
+		int second = time1.getSecond();
+
+		int i = second - time2.getSecond();
+		if (i < 0 ) {
+			System.out.println(i + 60);
+			minute = minute - 1;
+		} else {
+			System.out.println(i);
+		}
+
+		int i2 = minute - time2.getMinute();
+		if (i2 < 0 ) {
+			System.out.println(i2 + 60);
+			hours = hours - 1;
+		} else {
+			System.out.println(i2);
+		}
+
+		int i3 = hours - time2.getHour();
+		if (i3 < 0 ) {
+			System.out.println(i3 + 24);
+			day = day - 1;
+		} else {
+			System.out.println(i3);
+		}
+
+		int i4 = day - time2.getDayOfMonth();
+		if (i < 0 ) {
+			LocalDate with = LocalDate.of(time1.getYear(), time1.getMonthValue(), 1).with(TemporalAdjusters.lastDayOfMonth());
+			int dayOfMonth = with.getDayOfMonth();
+			System.out.println(i4 + dayOfMonth);
+			month = month - 1;
+		} else {
+			System.out.println(i4);
+		}
+
+		int i5 = month - time2.getMonthValue();
+		if (i < 0 ) {
+			System.out.println(i5 + 12);
+			year = year - 1;
+		} else {
+			System.out.println(i5);
+		}
+
+		int i6 = year - time2.getYear();
+		System.out.println(i6);
+	}
 
 }
