5cf238fd8aa7e70071e695bc9890d689fad93a30
keqi <iskeqi@126.com>
2021-04-23 10:25:15 +0800

    :sparkles: java-core相关API使用示例

---
+++ 039-core-java/time/src/main/java/dfadfa.java
@@ -0,0 +1,13 @@
+/**
+ * @author keqi
+ */
+public class dfadfa implements Ingtegsags {
+	@Override
+	public void test3() {
+		System.out.println("test3");
+	}
+
+	public static void main(String[] args) {
+		dfadfa dfadfa = new dfadfa();
+	}
+}
