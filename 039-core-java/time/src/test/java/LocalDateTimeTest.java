25b1297f760e2640cd7c855e1044af1cb491e09d
keqi <iskeqi@126.com>
2021-05-15 16:17:28 +0800

    :sparkles: java-core相关API使用示例

--- 039-core-java/time/src/test/java/LocalDateTimeTest.java
+++ 039-core-java/time/src/test/java/LocalDateTimeTest.java
@@ -1,8 +1,7 @@
 import org.junit.Test;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author keqi
@@ -19,7 +18,8 @@ public class LocalDateTimeTest {
 		System.out.println(zonedDateTime);
 
 		LocalDateTime of = LocalDateTime.of(2020, 1, 1, 1,1,1);
-
-
+		
+		System.out.println(LocalDateTime.of(LocalDate.now(), LocalTime.of(0, 0, 0)).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+		System.out.println(LocalDateTime.of(LocalDate.now(), LocalTime.of(23, 59, 59)).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
 	}
 }
