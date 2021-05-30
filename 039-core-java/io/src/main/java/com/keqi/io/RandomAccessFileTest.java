c4354121c549779eb65abde1627f473b7aae14b3
keqi <iskeqi@126.com>
2021-05-30 22:00:45 +0800

    :sparkles: java-core相关API使用示例

---
+++ 039-core-java/io/src/main/java/com/keqi/io/RandomAccessFileTest.java
@@ -0,0 +1,15 @@
+package com.keqi.io;
+
+import java.io.RandomAccessFile;
+
+/**
+ * @author keqi
+ */
+public class RandomAccessFileTest {
+	
+	public static void main(String[] args) {
+		RandomAccessFile randomAccessFile;
+	}
+	
+	
+}
