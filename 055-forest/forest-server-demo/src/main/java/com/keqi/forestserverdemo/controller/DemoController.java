5f8ba6a34c34d77476ebe71d0c4a0432d1c2ea32
keqi <iskeqi@126.com>
2021-07-31 17:08:50 +0800

    :sparkles: forest 使用示例

--- 055-forest/forest-server-demo/src/main/java/com/keqi/forestserverdemo/controller/DemoController.java
+++ 055-forest/forest-server-demo/src/main/java/com/keqi/forestserverdemo/controller/DemoController.java
@@ -2,6 +2,8 @@ package com.keqi.forestserverdemo.controller;
 
 import com.keqi.forestserverdemo.response.ResultEntity;
 import com.keqi.forestserverdemo.response.ResultEntityBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +15,15 @@ import java.util.Map;
 @RestController
 public class DemoController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/test1")
     public ResultEntity test1(String username, String password) {
         Map<String, Object> r = new HashMap<>();
         r.put("username", username);
         r.put("password", password);
+        r.put("port", environment.getProperty("server.port"));
         double random = Math.random();
         if (random > 0.5) {
             return ResultEntityBuilder.success(r);
