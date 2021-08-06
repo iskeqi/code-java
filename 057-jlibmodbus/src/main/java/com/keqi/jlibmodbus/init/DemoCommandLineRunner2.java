a84ba31da40ccf8b864be7180a99926704d9b1f1
keqi <iskeqi@126.com>
2021-08-06 18:19:11 +0800

    :memo: jlibmodbus 类库使用示例

--- 057-jlibmodbus/src/main/java/com/keqi/jlibmodbus/init/DemoCommandLineRunner2.java
+++ 057-jlibmodbus/src/main/java/com/keqi/jlibmodbus/init/DemoCommandLineRunner2.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 
 import java.net.InetAddress;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -18,68 +20,87 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class DemoCommandLineRunner2 implements CommandLineRunner {
 
+    private final Object obj = new Object();
+
     @Override
     public void run(String... args) throws Exception {
         // 通过定时任务线程池的方式开启定时任务
         ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
+        Map<String, ModbusMaster> modbusMasterMap = new HashMap<>();
+
+        TcpParameters tcpParameters1 = new TcpParameters();
+        tcpParameters1.setHost(InetAddress.getByName("127.0.0.1"));
+        tcpParameters1.setKeepAlive(true);
+        tcpParameters1.setPort(9090);
+        ModbusMaster master1 = ModbusMasterFactory.createModbusMasterTCP(tcpParameters1);
+        Modbus.setAutoIncrementTransactionId(true);
+
+        TcpParameters tcpParameters2 = new TcpParameters();
+        tcpParameters2.setHost(InetAddress.getByName("127.0.0.1"));
+        tcpParameters2.setKeepAlive(true);
+        tcpParameters2.setPort(9091);
+        ModbusMaster master2 = ModbusMasterFactory.createModbusMasterTCP(tcpParameters2);
+        Modbus.setAutoIncrementTransactionId(true);
+
+        modbusMasterMap.put("127.0.0.1:9090", master1);
+        modbusMasterMap.put("127.0.0.1:9091", master2);
+
+        for (Map.Entry<String, ModbusMaster> entry : modbusMasterMap.entrySet()) {
+            ModbusMaster master = entry.getValue();
+            if (!master.isConnected()) {
+                master.connect();
+            }
+        }
+
         scheduledExecutorService.scheduleWithFixedDelay(() -> {
             try {
-                TcpParameters tcpParameters = new TcpParameters();
-                InetAddress address = InetAddress.getByName("127.0.0.1");
-                tcpParameters.setHost(address);
-                tcpParameters.setKeepAlive(true);
-                tcpParameters.setPort(Modbus.TCP_PORT);
+                synchronized (obj) {
+                    // 如果有多个 slave 设备呢？直接在内存中维护多个 ModbusMaster 对象吗？还是 ModbusSlave 对象
+                    // 当然是 ModbusMaster 对象啦
+                    // ModbusSlave slave = ModbusSlaveFactory.createModbusSlaveTCP(tcpParameters);
 
-                ModbusMaster master = ModbusMasterFactory.createModbusMasterTCP(tcpParameters);
-                Modbus.setAutoIncrementTransactionId(true);
+                    for (Map.Entry<String, ModbusMaster> entry : modbusMasterMap.entrySet()) {
+                        ModbusMaster master = entry.getValue();
 
-                // 如果有多个 slave 设备呢？直接在内存中维护多个 ModbusMaster 对象吗？还是 ModbusSlave 对象
-                // 当然是 ModbusMaster 对象啦
-                // ModbusSlave slave = ModbusSlaveFactory.createModbusSlaveTCP(tcpParameters);
+                        int slaveId = 1;//从机地址
+                        int offset = 0;//寄存器读取开始地址
+                        int quantity = 10;//读取的寄存器数量
 
-                if (!master.isConnected()) {
-                    master.connect();
-                }
-
-                int slaveId = 1;//从机地址
-                int offset = 0;//寄存器读取开始地址
-                int quantity = 10;//读取的寄存器数量
-
-                // 读取线圈寄存器
-                System.out.println("==================");
+                        // 读取线圈寄存器
 
                     /*boolean[] coils = master.readCoils(slaveId, offset, quantity);
                     System.out.println(Arrays.toString(coils));*/
 
-                // 读取离散寄存器
+                        // 读取离散寄存器
                     /*boolean[] discreteInputs = master.readDiscreteInputs(slaveId, offset, quantity);
                     System.out.println(Arrays.toString(discreteInputs));*/
 
-                // 读取保持寄存器
-                int[] holdingRegisters = master.readHoldingRegisters(slaveId, offset, quantity);
-                System.out.println(Arrays.toString(holdingRegisters));
+                        // 读取保持寄存器
+                        int[] holdingRegisters = master.readHoldingRegisters(slaveId, offset, quantity);
+                        System.out.println(Arrays.toString(holdingRegisters) + "->" + entry.getKey());
 
-                // 读取输入寄存器
+                        // 读取输入寄存器
                     /*int[] inputRegisters = master.readInputRegisters(slaveId, offset, quantity);
                     System.out.println(Arrays.toString(inputRegisters));*/
 
-                // 写单个线圈寄存器
-                // master.writeSingleCoil();
+                        // 写单个线圈寄存器
+                        // master.writeSingleCoil();
 
-                // 写多个线圈寄存器
-                // master.writeMultipleCoils();
+                        // 写多个线圈寄存器
+                        // master.writeMultipleCoils();
 
-                // 写单个保持寄存器
-                // master.writeSingleRegister();
+                        // 写单个保持寄存器
+                        // master.writeSingleRegister();
 
-                // 写多个保持寄存器
-                // master.writeMultipleRegisters();
-
-                System.out.println("==================");
+                        // 写多个保持寄存器
+                        // master.writeMultipleRegisters();
+                    }
+                    System.out.println("----------------->");
+                }
             } catch (Throwable e) {
                 log.error(e.getMessage());
             }
-        }, 1, 2, TimeUnit.SECONDS);
+        }, 1, 5, TimeUnit.SECONDS);
     }
 }
