4d25a68d06bc4234a9535152188bf540ec226a71
keqi <iskeqi@126.com>
2021-08-12 18:18:35 +0800

    :memo: netty 使用示例

--- 059-netty/netty-springboot-client/src/main/java/com/keqi/nettyspringbootclient/netty/SimpleChatClientHandler.java
+++ 059-netty/netty-springboot-client/src/main/java/com/keqi/nettyspringbootclient/netty/SimpleChatClientHandler.java
@@ -1,5 +1,6 @@
 package com.keqi.nettyspringbootclient.netty;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
@@ -9,6 +10,9 @@ public class SimpleChatClientHandler extends SimpleChannelInboundHandler<String>
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String s) throws Exception {
+        Channel channel = ctx.channel();
+        // 从 Map<Channel, String> 对象中根据 Channel 对象找到对应的远程服务器地址
+
         boolean r = Objects.equals(s, ctx.channel().id().asLongText() + "\n");
         System.out.println(Thread.currentThread().getId() + " " + r + " " + s + " " + ctx.channel().id().asLongText());
     }
