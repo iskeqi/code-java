5753935cb83ecd70a5b88b24d6bbc952060ce3fd
keqi <iskeqi@126.com>
2021-08-26 14:38:01 +0800

    :memo: project-seed-4 使用示例

--- 056-project-seed-4/src/main/java/com/keqi/seed/Application.java
+++ 056-project-seed-4/src/main/java/com/keqi/seed/Application.java
@@ -1,12 +1,10 @@
 package com.keqi.seed;
 
 import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-
 @SpringBootApplication
 public class Application {
 
