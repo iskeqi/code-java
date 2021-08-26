d4284a6f2b114a1f9a86b59325236a02e24d4417
keqi <iskeqi@126.com>
2021-08-26 18:58:50 +0800

    :memo: minio 使用示例

--- 060-minio/src/main/java/com/keqi/minio/DemoController.java
+++ 060-minio/src/main/java/com/keqi/minio/DemoController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZonedDateTime;
-import java.util.UUID;
 
 @RestController
 public class DemoController {
@@ -18,7 +17,8 @@ public class DemoController {
 
         2、删除文件：客户端请求应用服务器要删除指定文件，应用服务器使用 removeObject 向 minio 服务发送命令，直接删除文件
 
-        3、上传文件：客户端请求应用服务器要上传指定文件，应用服务器返回对应的信息，客户端直接上传文件即可
+        3、上传文件：客户端请求应用服务器要上传指定文件，应用服务器使用presignedPutObject向 minio服务发送命令，
+         返回对应的信息（一个带了很多参数的 url），客户端使用此url发送put请求，并在body中带上文集爱你内容
 
      */
 
@@ -39,22 +39,55 @@ public class DemoController {
                 new MinioClient("http://192.168.74.132:9000", "minioadmin", "minioadmin");
         PostPolicy postPolicy = new PostPolicy("tese1", objectName, ZonedDateTime.now().plusDays(1));
 
+        // 返回值是一个 url(带上了很多参数的) ，直接向此 url 发送请求(PUT)，并且 body 里面带上二进制文件就行（）
+
+        /*
+            js 使用示例
+
+            var data = "<file contents here>";
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function() {
+  if(this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("PUT", "http://192.168.74.132:9000/tese1/fms-4.8.3.exe?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=minioadmin%252F20210826%252Fus-east-1%252Fs3%252Faws4_request&X-Amz-Date=20210826T105014Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=cc3489d0350cee8e0ce3af5c10395d232d98d119a33d2ec711bb83bfcbfbe4d1");
+xhr.setRequestHeader("Content-Type", "application/x-msdos-program");
+
+xhr.send(data);
+
+
+         */
+
+
+
+        return minioClient.presignedPutObject("tese1", objectName);
+
+
+
+
+
         /*Map<String, String> stringStringMap = minioClient.presignedPostPolicy(postPolicy);
         for (Map.Entry<String, String> entry : stringStringMap.entrySet()) {
             // 使用 curl 命令带上这些参数，就可以进行文件上传
 
             // 对于客户端调用，具体应该如何进行传参呢？？？当然是 multipart/form-data 啦 方式进行上传啦，把这几个参数都给带上就行
             // 使用 postman 进行测试后，发现是正确的 todo 未找到解决办法哦，这个必须要知道如何解决
+            // 上传文件的那个字段叫做 file （https://juejin.cn/post/6978304433809522719）
             System.out.print(" -F " + entry.getKey() + "=" + entry.getValue());
         }*/
         // 换成 put 请求（还是没有成功）
         // String url = minioClient.presignedPutObject("tese1", objectName, 60 * 60 * 24);
 
         // 直接上传本地文件(这种方法肯定是没有问题的)
-        String name = UUID.randomUUID().toString() + ".txt";
-        minioClient.putObject("tese1", name, "C:\\Users\\keqi\\Desktop\\本周.txt", null);
+       /* String name = UUID.randomUUID().toString() + ".txt";
+        minioClient.putObject("tese1", name, "C:\\Users\\keqi\\Desktop\\本周.txt", null);*/
 
-        return "success";
+        /*return stringStringMap;*/
     }
 
     @GetMapping("delete")
