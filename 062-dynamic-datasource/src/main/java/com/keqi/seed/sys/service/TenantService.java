6cfcbcb05801f001afa5c2915e584157ee43a292
keqi <iskeqi@126.com>
2021-08-31 17:20:16 +0800

    :memo: 基于动态数据源实现多租户 使用示例

--- 062-dynamic-datasource/src/main/java/com/keqi/seed/sys/service/TenantService.java
+++ 062-dynamic-datasource/src/main/java/com/keqi/seed/sys/service/TenantService.java
@@ -9,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class TenantService implements BaseService<TenantDO> {
 
@@ -16,13 +20,31 @@ public class TenantService implements BaseService<TenantDO> {
     private TenantMapper tenantMapper;
     @Autowired
     private DataSourceManager dataSourceManager;
+    private final List<Handler<TenantDO>> chainList = new ArrayList<>();
 
     @Override
     public TenantDO insert(TenantDO tenantDO) {
+        int i;
+        boolean isFail = false;
+        for (i = 0; i < chainList.size(); i++) {
+            Handler<TenantDO> handler = chainList.get(i);
+            if (!handler.execute(tenantDO)) {
+                isFail = true;
+                break;
+            }
+        }
+
+        if (isFail) {
+            // 进行回滚
+            for (int j = i; j >= 0; j--) {
+                Handler<TenantDO> handler = chainList.get(j);
+                handler.rollback(tenantDO);
+            }
+            throw new BusinessException("租户创建失败，请稍后重试");
+        }
+
+        /*// todo 待重构，切换成责任链模式
         String tenantIdentifier = tenantDO.getTenantIdentifier();
-
-        // todo 待重构，切换成责任链模式
-
         boolean result1 = true;
         try {
             // 新增租户记录
@@ -68,8 +90,7 @@ public class TenantService implements BaseService<TenantDO> {
                 tenantMapper.deleteById(tenantDO.getId());
             }
             throw new BusinessException("租户创建失败，请稍后重试");
-        }
-
+        }*/
         return tenantDO;
     }
 
@@ -94,5 +115,83 @@ public class TenantService implements BaseService<TenantDO> {
     @Override
     public TenantDO getById(String id) {
         return tenantMapper.selectById(id);
+    }
+
+    @PostConstruct
+    public void init() {
+        chainList.add(new Handler<TenantDO>() {
+            @Override
+            public boolean execute(TenantDO tenantDO) {
+                boolean result = true;
+                try {
+                    // 新增租户记录
+                    tenantMapper.insert(tenantDO);
+                } catch (Throwable e) {
+                    result = false;
+                }
+                return result;
+            }
+
+            @Override
+            public void rollback(TenantDO tenantDO) {
+                tenantMapper.deleteById(tenantDO.getId());
+            }
+        });
+
+        chainList.add(new Handler<TenantDO>() {
+            @Override
+            public boolean execute(TenantDO tenantDO) {
+                boolean result = true;
+                try {
+                    // 创建租户对应的 db
+                    tenantMapper.createDatabase(tenantDO.getTenantIdentifier());
+                } catch (Throwable e) {
+                    result = false;
+                }
+                return result;
+            }
+
+            @Override
+            public void rollback(TenantDO tenantDO) {
+                tenantMapper.dropDatabase(tenantDO.getTenantIdentifier());
+            }
+        });
+
+        chainList.add(new Handler<TenantDO>() {
+            @Override
+            public boolean execute(TenantDO tenantDO) {
+                // 在新的数据库中执行 sql 脚本
+                return dataSourceManager.runSQLScriptFile(tenantDO.getTenantIdentifier());
+            }
+
+            @Override
+            public void rollback(TenantDO tenantDO) {
+                // 返回值为 false 时，已经进行回滚了，这里无需手动进行回滚
+            }
+        });
+
+        chainList.add(new Handler<TenantDO>() {
+            @Override
+            public boolean execute(TenantDO tenantDO) {
+                boolean result = true;
+                try {
+                    dataSourceManager.createDataSource(tenantDO.getTenantIdentifier());
+                } catch (Throwable e) {
+                    result = false;
+                }
+                return result;
+            }
+
+            @Override
+            public void rollback(TenantDO tenantDO) {
+                // 数据源添加失败时，同样无需进行回滚
+            }
+        });
+    }
+
+    private interface Handler<T> {
+        boolean execute(T tenantDO);
+
+        void rollback(T tenantDO);
     }
 }
