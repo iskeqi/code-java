e6c12391d0ac95e4325fa4dc4c70207c28340477
keqi <iskeqi@126.com>
2021-09-15 10:41:13 +0800

    :sparkles: java-core相关API使用示例

--- 039-core-java/thread/src/main/java/com/keqi/thread/waitnotify/Main.java
+++ 039-core-java/thread/src/main/java/com/keqi/thread/waitnotify/Main.java
@@ -12,5 +12,7 @@ public class Main {
         // 两个 int 类型的数字，通过字符串的方式拼接出来的数字比 bigint unsigned 都要大很多
         System.out.println("18446744073709551615");
         System.out.println(Integer.MAX_VALUE + "" + Integer.MAX_VALUE);
+
+        String a = "18446744073709551615";
     }
 }
