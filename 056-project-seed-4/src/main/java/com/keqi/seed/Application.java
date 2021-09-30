10b462f1039b7bd26374a409f0efc2453a39fef5
keqi <iskeqi@126.com>
2021-09-30 11:36:26 +0800

    :memo: project-seed-4 使用示例

--- 056-project-seed-4/src/main/java/com/keqi/seed/Application.java
+++ 056-project-seed-4/src/main/java/com/keqi/seed/Application.java
@@ -1,30 +1,18 @@
 package com.keqi.seed;
 
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.UUID;
-
-/*@Slf4j*/
-@Log4j2
+@Slf4j
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
-        // log.debug((Supplier<String>) () -> "http://localhost:21803/seed/doc.html");
-        // log.debug((Supplier<String>) Application::f);
-        // log.debug(f());
-        /*log.info((Supplier<String>) Application::f);*/
-
-        log.debug(Application::f);
-    }
-
-    private static String f() {
-        UUID uuid = UUID.randomUUID();
-        System.out.println(uuid);
-        return uuid.toString();
+        if (log.isDebugEnabled()) {
+            log.debug("http://localhost:21803/seed/doc.html");
+        }
     }
 }
