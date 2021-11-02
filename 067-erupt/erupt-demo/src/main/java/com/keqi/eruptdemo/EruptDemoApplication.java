2d54d78724de4a8f9c3be45f3f4bf7d0665796c9
keqi <iskeqi@126.com>
2021-11-02 09:17:46 +0800

    :memo: erupt 框架使用示例

---
+++ 067-erupt/erupt-demo/src/main/java/com/keqi/eruptdemo/EruptDemoApplication.java
@@ -0,0 +1,17 @@
+package com.keqi.eruptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import xyz.erupt.core.annotation.EruptScan;
+
+@EntityScan
+@EruptScan
+@SpringBootApplication
+public class EruptDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EruptDemoApplication.class, args);
+    }
+
+}
---
+++ 067-erupt/erupt-demo/src/main/java/com/keqi/eruptdemo/domain/Simple.java
@@ -0,0 +1,56 @@
+package com.keqi.eruptdemo.domain;
+
+import org.hibernate.annotations.GenericGenerator;
+import xyz.erupt.annotation.Erupt;
+import xyz.erupt.annotation.EruptField;
+import xyz.erupt.annotation.sub_field.Edit;
+import xyz.erupt.annotation.sub_field.View;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author keqi
+ */
+@Erupt(name = "简单的例子")
+@Table(name = "demo_simple")
+@Entity
+public class Simple {
+
+    //主键
+    @Id
+    @GeneratedValue(generator = "generator")
+    @GenericGenerator(name = "generator", strategy = "native")
+    @Column(name = "id")
+    @EruptField
+    private Long id;
+
+    //文本输入
+    @EruptField(
+            views = @View(title = "文本"),
+            edit = @Edit(title = "文本")
+    )
+    private String input;
+
+    //数值输入
+    @EruptField(
+            views = @View(title = "数值"),
+            edit = @Edit(title = "数值")
+    )
+    private Integer number;
+
+    //布尔选择
+    @EruptField(
+            views = @View(title = "布尔"),
+            edit = @Edit(title = "布尔")
+    )
+    private Boolean bool;
+
+    //时间选择
+    @EruptField(
+            views = @View(title = "时间"),
+            edit = @Edit(title = "时间")
+    )
+    private Date date;
+
+}
