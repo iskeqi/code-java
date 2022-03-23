2e37e221096297e6e348af1a345f9623993241e1
keqi <iskeqi@126.com>
2022-03-23 21:48:36 +0800

    mqtt 类库使用案例

--- 078-mqtt/eclipse-paho-client/src/main/java/com/keqi/mqtt/connect/ConnectTest.java
+++ 078-mqtt/eclipse-paho-client/src/main/java/com/keqi/mqtt/connect/ConnectTest.java
@@ -7,19 +7,15 @@ public class ConnectTest {
 
     public static void main(String[] args) {
 
-        String topic        = "MQTT Examples";
-        String content      = "Message from MqttPublishSample";
-        int qos             = 2;
-        String broker       = "tcp://mqtt.eclipseprojects.io:1883";
-        String clientId     = "JavaSample";
+        String topic = "MQTT Examples";
+        String content = "Message from MqttPublishSample";
+        int qos = 2;
+        String broker = "tcp://mqtt.eclipseprojects.io:1883";
+        String clientId = "JavaSample";
         MemoryPersistence persistence = new MemoryPersistence();
 
         try {
             MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
-
-
-
-
 
 
             // 设置connect 属性
@@ -47,15 +43,17 @@ public class ConnectTest {
 
                 }
             });
-            System.out.println("Connecting to broker: "+broker);
+            System.out.println("Connecting to broker: " + broker);
             sampleClient.connect(connOpts);
             System.out.println("Connected");
 
 
             // 发送一个 qos为2的消息到 broker 中
-            System.out.println("Publishing message: "+content);
+            System.out.println("Publishing message: " + content);
             MqttMessage message = new MqttMessage(content.getBytes());
             message.setQos(qos);
+//            message.setRetained(true); // 是否发送保留消息
+
             sampleClient.publish(topic, message);
             System.out.println("Message published");
 
@@ -70,13 +68,13 @@ public class ConnectTest {
 
             System.out.println("Disconnected");
             System.exit(0);
-        } catch(MqttException me) {
+        } catch (MqttException me) {
 
-            System.out.println("reason "+me.getReasonCode());
-            System.out.println("msg "+me.getMessage());
-            System.out.println("loc "+me.getLocalizedMessage());
-            System.out.println("cause "+me.getCause());
-            System.out.println("excep "+me);
+            System.out.println("reason " + me.getReasonCode());
+            System.out.println("msg " + me.getMessage());
+            System.out.println("loc " + me.getLocalizedMessage());
+            System.out.println("cause " + me.getCause());
+            System.out.println("excep " + me);
 
             me.printStackTrace();
         }
