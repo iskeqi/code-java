2507ee8bf4347c3461eecc16107863e206ff51c7
keqi <iskeqi@126.com>
2022-04-13 23:43:50 +0800

    :memo: springboot admin 使用示例

---
+++ 079-spring-boot-admin/admin-server/src/test/java/com/keqi/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keqi.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
