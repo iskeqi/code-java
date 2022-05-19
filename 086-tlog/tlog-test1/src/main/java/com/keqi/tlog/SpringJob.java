86c80a019132d97f1b24dd7705a8e469ed81b36b
keqi <iskeqi@126.com>
2022-05-19 22:09:45 +0800

    :sparkles: TLog 使用示例

---
+++ 086-tlog/tlog-test1/src/main/java/com/keqi/tlog/SpringJob.java
@@ -0,0 +1,26 @@
+package com.keqi.tlog;
+
+import com.yomahub.tlog.core.annotation.TLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+@EnableScheduling
+public class SpringJob {
+
+	//@Scheduled(fixedRate = 100)
+	public void init() {
+		log.info("==========");
+		demo1(UUID.randomUUID().toString());
+	}
+
+	@TLogAspect({"id"})
+	public void demo1(String id) {
+		log.info(UUID.randomUUID().toString());
+	}
+}
