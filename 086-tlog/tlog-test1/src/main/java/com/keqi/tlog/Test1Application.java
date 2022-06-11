63124b2860bf484e3454c1301c5291dd7bf1d6cd
keqi <iskeqi@126.com>
2022-06-11 21:45:34 +0800

    :sparkles: TLog 使用示例

--- 086-tlog/tlog-test1/src/main/java/com/keqi/tlog/Test1Application.java
+++ 086-tlog/tlog-test1/src/main/java/com/keqi/tlog/Test1Application.java
@@ -1,26 +1,21 @@
 package com.keqi.tlog;
 
-import com.dtflys.forest.springboot.annotation.ForestScan;
-import com.yomahub.tlog.core.annotation.TLogAspect;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-@ForestScan(basePackages = "com.keqi.tlog")
+//@ForestScan(basePackages = "com.keqi.tlog")
 @RestController
 @Slf4j
 @SpringBootApplication
 public class Test1Application {
 
-	@Autowired
-	private MyClient myClient;
+//	@Autowired
+//	private MyClient myClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test1Application.class, args);
@@ -35,10 +30,10 @@ public class Test1Application {
 //		});
 	}
 
-	@TLogAspect({"id"})
+//	@TLogAspect({"id"})
 	@GetMapping("/demo")
 	public void demo(String id) {
 		log.info(UUID.randomUUID().toString());
-		myClient.helloForest(UUID.randomUUID().toString());
+//		myClient.helloForest(UUID.randomUUID().toString());
 	}
 }
